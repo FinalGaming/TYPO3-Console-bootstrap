0913f6e79417d356d95ab119e9cabe76dee28280
Daniel Ruf <daniel@daniel-ruf.de>
2017-11-27 21:19:23 +0100

    typo3cms was renamed to typo3console

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ username=${user#$prefix}
 userdir=/www/htdocs/${username}
 cd ${userdir}
 git clone https://github.com/TYPO3-Console/TYPO3-Console.git
-cp TYPO3-Console/Scripts/typo3cms ${userdir}/bin
-echo "alias typo3cms='${userdir}/bin/typo3cms'" >> ${userdir}/.user_bashrc
+cp TYPO3-Console/Scripts/typo3console ${userdir}/bin
+echo "alias typo3console='${userdir}/bin/typo3console'" >> ${userdir}/.user_bashrc
 rm -rf TYPO3-Console
 source /.bashrc
