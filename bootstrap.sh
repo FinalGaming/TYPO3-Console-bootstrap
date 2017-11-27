7a5f3587aa5c1b4949f06a11f5e25c70c656e730
Daniel Ruf <daniel@daniel-ruf.de>
2017-11-27 21:23:02 +0100

    also copy php file

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ userdir=/www/htdocs/${username}
 cd ${userdir}
 git clone https://github.com/TYPO3-Console/TYPO3-Console.git
 cp TYPO3-Console/Scripts/typo3console ${userdir}/bin
+cp TYPO3-Console/Scripts/typo3console.php ${userdir}/bin
 echo "alias typo3console='${userdir}/bin/typo3console'" >> ${userdir}/.user_bashrc
 rm -rf TYPO3-Console
 source /.bashrc
